8e4b836c8406a62a24bbd55fffcb1d5b9b33fe69
Felipe Maciel <39608547+Digite1Nome@users.noreply.github.com>
2025-03-04 21:42:03 -0300

    nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   head: {
     meta: [
-      { name: 'facebook-domain-verification', content: 'dwkl9e50kfx9phaekgdn91o3awo23y' }
+      { name: "facebook-domain-verification", content: "dwkl9e50kfx9phaekgdn91o3awo23y" }
     ]
   }
 };
